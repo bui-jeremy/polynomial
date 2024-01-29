4b4e39fa9e8fa8114051d9ca59fea722b02196c8
bui-jeremy <jeremybui2002@gmail.com>
2024-01-28 22:28:11 -0500

    cs 506 excercise part c

--- polynomial.py
+++ polynomial.py
@@ -5,42 +5,29 @@ class X:
     def __repr__(self):
         return "X"
 
+    def evaluate(self, value):
+        return value
+
 class Int:
     def __init__(self, i):
         self.i = i
-    
+
     def __repr__(self):
         return str(self.i)
+
+    def evaluate(self, value):
+        return self.i
 
 class Add:
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
-    
+
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
 
-class Mul:
-    def __init__(self, p1, p2):
-        self.p1 = p1
-        self.p2 = p2
-    
-    def __repr__(self):
-        if isinstance(self.p1, Add):
-            if isinstance(self.p2, Add):
-                 return "( " + repr(self.p1) + " ) * ( " + repr(self.p2) + " )"
-            return "( " + repr(self.p1) + " ) * " + repr(self.p2)
-        if isinstance(self.p2, Add):
-            return repr(self.p1) + " * ( " + repr(self.p2) + " )"
-        return repr(self.p1) + " * " + repr(self.p2)
-
-class Div:
-    def __init__(self, p1, p2):
-        self.p1 = p1
-        self.p2 = p2
-
-    def __repr__(self):
-        return "( " + repr(self.p1) + " ) / ( " + repr(self.p2) + " )"
+    def evaluate(self, value):
+        return self.p1.evaluate(value) + self.p2.evaluate(value)
 
 class Sub:
     def __init__(self, p1, p2):
@@ -50,5 +37,47 @@ class Sub:
     def __repr__(self):
         return repr(self.p1) + " - ( " + repr(self.p2) + " )"
 
-poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
-print(poly)
+    def evaluate(self, value):
+        return self.p1.evaluate(value) - self.p2.evaluate(value)
+
+class Mul:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def __repr__(self):
+        if isinstance(self.p1, Add) or isinstance(self.p1, Sub):
+            repr_p1 = "( " + repr(self.p1) + " )"
+        else:
+            repr_p1 = repr(self.p1)
+
+        if isinstance(self.p2, Add) or isinstance(self.p2, Sub):
+            repr_p2 = "( " + repr(self.p2) + " )"
+        else:
+            repr_p2 = repr(self.p2)
+
+        return repr_p1 + " * " + repr_p2
+
+    def evaluate(self, value):
+        return self.p1.evaluate(value) * self.p2.evaluate(value)
+
+class Div:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def __repr__(self):
+        return "( " + repr(self.p1) + " ) / ( " + repr(self.p2) + " )"
+
+    def evaluate(self, value):
+        if self.p2.evaluate(value) == 0:
+            raise ValueError("Division by zero is not allowed")
+        return self.p1.evaluate(value) / self.p2.evaluate(value)
+
+poly = Add(Add(Int(4), Int(3)), Add(X(), Mul(Int(1), Add(Mul(X(), X()), Int(1)))))
+print("Polynomial Representation:", poly)
+print("Polynomial Evaluation for X = -1:", poly.evaluate(-1))
+
+div_by_zero = Div(Int(1), Int(0))
+print("1 / 0:", div_by_zero.evaluate(0))
+
